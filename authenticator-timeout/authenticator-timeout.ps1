57de41b56914827f4b087b6d4d6a13b69565442e
Linus Niederer <linus.niederer@bluemail.ch>
2021-12-13 13:36:38 +0100

    Update authenticator-timeout.ps1

--- authenticator-timeout/authenticator-timeout.ps1
+++ authenticator-timeout/authenticator-timeout.ps1
@@ -11,24 +11,31 @@ Class Authenticator {
     [string] $processName   = "CAA"
     [bool] $autostart       = $true
 
-    [bool] $isRunning
-
-
     # Constructor of class
     Authenticator() {
-        # nothing to do here
+        while( $true ) {
+            $this.getProcess()
+            Start-Sleep -s 60
+        }
     }
 
     # Function to get process information
     [void] getProcess() {
 
+        $process = Get-Process | Where-Object { $_.ProcessName -eq $this.processName }
+        $startTime = $process.StartTime
+        $currentTime = (Get-Date)
 
+        $difference = New-TimeSpan -Start $startTime -End $currentTime
 
+        if( $difference.Hours -gt 8 ) {
+            Write-Host "Kill process with ID $($process.ID)"
+            Stop-Process -Id $process.Id
 
+        } else {
+            Write-Host "Nothing to do process is running for $difference"
+        }
     }
-
-
-
 }
 
 
